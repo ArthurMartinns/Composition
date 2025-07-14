a3b4fc7187e9cd2bdf852c704c6a53bc3169cd9d
ArthurMartinns <arthurmartiinscontato@gmail.com>
2025-07-14 17:04:17 -0300

    feat: starting worker class

--- src/entities/Worker.java
+++ src/entities/Worker.java
@@ -1,4 +1,49 @@
 package entities;
 
+import enums.WorkerLevelEnum;
+
 public class Worker {
+    private String name;
+    private WorkerLevelEnum level;
+    private Double baseSalary;
+
+    public Worker(){}
+
+    public Worker(String name, WorkerLevelEnum level, Double baseSalary) {
+        this.name = name;
+        this.level = level;
+        this.baseSalary = baseSalary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String newName) {
+        name = newName;
+    }
+
+    public Double getBaseSalary() {
+        return baseSalary;
+    }
+
+    public void setBaseSalary(Double newBaseSalary) {
+        baseSalary = newBaseSalary;
+    }
+
+    public WorkerLevelEnum getLevel() {
+        return level;
+    }
+
+    public void setLevel(WorkerLevelEnum newLevel) {
+        level = newLevel;
+    }
+
+    public void addContract(HourContract contract) {}
+
+    public void removeContract(HourContract contact){}
+
+    public Double income(Integer year, Integer month) {
+        return 0.0;
+    }
 }
